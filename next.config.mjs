1f325fca5672ed02392e2dafb5e67faa181f4ac5
Physics-Computer <ms_0710_@naver.com>
2025-04-07 15:44:59 +0900

    배포용 저장

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // 여기 추가!
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, './');
+    return config;
+  },
 };
 
 export default nextConfig;
