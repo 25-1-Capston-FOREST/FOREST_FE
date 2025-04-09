b526470b0d9837ee980855c2d00e9d8f6c223fda
Physics-Computer <ms_0710_@naver.com>
2025-04-09 17:36:28 +0900

    배포용으로 수정

--- src/lib/axios.tsx
+++ src/lib/axios.tsx
@@ -3,6 +3,9 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://capston-forest.duckdns.org",
   withCredentials: true, // 필요 시
+  headers: {
+    "Content-Type": "application/json", // 이거 꼭 있어야 해!
+  },
 });
 
 export default instance;
