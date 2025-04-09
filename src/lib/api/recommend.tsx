b102ffc4457b8738b021fa231d11c6528e8ee80d
Physics-Computer <ms_0710_@naver.com>
2025-04-09 19:48:37 +0900

    추천 리스트 연결

--- src/lib/api/recommend.tsx
+++ src/lib/api/recommend.tsx
@@ -2,5 +2,6 @@ import instance from "@/lib/axios";
 
 export const getRecommendation = async () => {
   const response = await instance.get("/api/recommendation");
+  console.log("API 응답 데이터:", response.data)
   return response.data.recommendations;
 };
