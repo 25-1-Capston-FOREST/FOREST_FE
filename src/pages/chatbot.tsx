29dc9e4f3bcf7827fe9b62ec804ca24506a3d5dc
Physics-Computer <ms_0710_@naver.com>
2025-06-02 15:30:58 +0900

    fix: 챗봇 페이지 디자인 수정 - 입력창

--- src/pages/chatbot.tsx
+++ src/pages/chatbot.tsx
@@ -70,7 +70,7 @@ export default function Chatbot() {
   }, [messages]);
 
   return (
-    <div className="mb-[50px] pt-[10px] pb-[90px] px-4 bg-white min-h-[calc(100vh-300px)] flex flex-col items-center">
+    <div className="mb-[50px] pt-[10px] pb-[60px] px-4 bg-white min-h-[calc(100vh-300px)] flex flex-col items-center">
       {/* 메시지 영역 */}
       <div
         ref={containerRef}
@@ -92,7 +92,7 @@ export default function Chatbot() {
       </div>
 
       {/* 입력창 */}
-      <div className="fixed bottom-0 flex left-1/2 -translate-x-1/2 z-10 w-full max-w-[550px] flex items-center space-x-2 px-4 bg-[#FFFFFF] h-[40px] pb-[70px]">
+      <div className="fixed bottom-0 flex left-1/2 -translate-x-1/2 z-10 w-full max-w-[550px] flex items-center space-x-2 px-4 bg-[#FFFFFF] h-[40px] pb-[50px]">
         <div className="flex flex-1 flex-row h-[40px] rounded-[10px] border items-center bg-white shadow-md px-2">
           <input
             className="flex-1 px-2 outline-none text-[14px]"
