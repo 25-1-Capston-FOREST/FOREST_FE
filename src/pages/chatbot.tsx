52a418ec48a5ecaed8f34859310b5e16b1b9259b
Physics-Computer <ms_0710_@naver.com>
2025-06-02 12:11:30 +0900

    fix: 챗봇 페이지 디자인 수정 - 메세지 스크롤

--- src/pages/chatbot.tsx
+++ src/pages/chatbot.tsx
@@ -88,7 +88,7 @@ useEffect(() => {
       </div>
 
       {/* 입력창 */}
-      <div className="fixed bottom-3 flex flex-col justify-start left-1/2 -translate-x-1/2 z-10 w-full max-w-[550px] flex items-center space-x-2 px-4 bg-[#FFFFFF] h-[80px]">
+      <div className="fixed bottom-3 flex left-1/2 -translate-x-1/2 z-10 w-full max-w-[550px] flex items-center space-x-2 px-4 bg-[#FFFFFF] h-[80px]">
         <div className="flex flex-1 flex-row h-[40px] rounded-[10px] border items-center bg-white shadow-md px-2">
           <input
             className="flex-1 px-2 outline-none text-[14px]"
