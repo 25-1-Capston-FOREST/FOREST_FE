3417fa5d734695ac91c85bdd255d6e56a78e45e6
Physics-Computer <ms_0710_@naver.com>
2025-06-02 11:37:14 +0900

    fix: 챗봇 페이지 디자인 수정 - 입력창 문제

--- src/pages/chatbot.tsx
+++ src/pages/chatbot.tsx
@@ -66,11 +66,11 @@ export default function Chatbot() {
   }, [messages])
 
   return (
-    <div className="mt-[-30px] flex flex-col items-center bg-white pt-30">
+    <div className="mt-[-30px] flex flex-col items-center bg-white ">
       {/* 메시지 영역 */}
       <div
         ref={containerRef}
-        className="flex flex-col justify-end w-full max-w-[527px] space-y-2 overflow-y-auto px-4 pt-4 pb-[150px]"
+        className="flex flex-col justify-end w-full max-w-[527px] space-y-2 overflow-y-auto px-4 pt-4 pb-[120px]"
         style={{
           minHeight: 150,
           maxHeight: "calc(100vh - 160px)",
